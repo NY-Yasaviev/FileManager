d2513550634bdd0349dc6587ecb680a8e47ac804
Kiku Mihail <CanisMajorisVY@yandex.ru>
2017-09-23 14:44:40 +0300

    canis_version

---
+++ File_manager.py
@@ -0,0 +1,27 @@
+import os
+memory = open('memory.txt')
+dict = {}
+for line in memory:
+    num = line.find(' ')
+    rline = line[num+1:]
+    rline = rline.strip()
+    line = line.split()[0]
+    dict[line] = rline
+memory.close()
+
+
+str = ''
+
+while str != 'exit':
+    str = input()
+
+    if str in dict:
+        os.startfile(dict[str], 'open')
+    else:
+        print('Введите путь к файлу:')
+        str1 = input()
+        memory = open('memory.txt', 'a')
+        memory.write('\n' + str + ' ' + str1)
+        dict[str] = str1
+        os.startfile(dict[str], 'open')
+        memory.close()
